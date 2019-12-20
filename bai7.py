59fc5bd313429698d6f549eaa6c783091fa9b1fe
than0mua <than0mua1994@gmail.com>
2019-12-20 04:28:39 -0800

    bai tap lan cuoi buoi 1

--- bai7.py
+++ bai7.py
@@ -3,17 +3,17 @@ if a<=100:
     print("tien dien truoc thue cua ban la:"+str(a*450))
     print("tien luong sau thue cua ban la:" + str(((a*450)/100)*110))
 elif a>100 & a<=200:
-    print("tien dien truoc thue cua ban la:" + str(a * 600))
-    print("tien luong sau thue cua ban la:" + str(((a * 600) / 100) * 110))
+    print("tien dien truoc thue cua ban la:" + str(a*450+(a-100) * 600))
+    print("tien luong sau thue cua ban la:" + str(((a*450+(a-100) * 600) / 100) * 110))
 elif a>200 & a<=300:
-    print("tien dien truoc thue cua ban la:" + str(a * 750))
-    print("tien luong sau thue cua ban la:" + str(((a * 750) / 100) * 110))
+    print("tien dien truoc thue cua ban la:" + str(a*450+(a-100) * 600+(a-200) * 750))
+    print("tien luong sau thue cua ban la:" + str(((a*450+(a-100) * 600+(a-200) * 750) / 100) * 110))
 elif a>300 & a<=500:
-    print("tien dien truoc thue cua ban la:" + str(a * 900))
-    print("tien luong sau thue cua ban la:" + str(((a * 900) / 100) * 110))
+    print("tien dien truoc thue cua ban la:" + str(a*450+(a-100) * 600+(a-200) * 750+(a-300) * 900))
+    print("tien luong sau thue cua ban la:" + str(((a*450+(a-100) * 600+(a-200) * 750+(a-300) * 900) / 100) * 110))
 elif a>500 & a<=1000:
-    print("tien dien truoc thue cua ban la:" + str(a * 1000))
-    print("tien luong sau thue cua ban la:" + str(((a * 1000) / 100) * 110))
+    print("tien dien truoc thue cua ban la:" + str(a*450+(a-100) * 600+(a-200) * 750+(a-300) * 900+(a-500) * 1000))
+    print("tien luong sau thue cua ban la:" + str(((a*450+(a-100) * 600+(a-200) * 750+(a-300) * 900+(a-500) * 1000) / 100) * 110))
 elif a>1000:
-    print("tien dien truoc thue cua ban la:" + str(a * 1200))
-    print("tien luong sau thue cua ban la:" + str(((a * 1200) / 100) * 110))
+    print("tien dien truoc thue cua ban la:" + str(a*450+(a-100) * 600+(a-200) * 750+(a-300) * 900+(a-500) * 1000+(a-1000) * 1200))
+    print("tien luong sau thue cua ban la:" + str(((a*450+(a-100) * 600+(a-200) * 750+(a-300) * 900+(a-500) * 1000+(a-1000) * 1200) / 100) * 110))
